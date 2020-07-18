f7f5dcac4b1ea3dd4a6af525130f16f0ab31147e
Nate Rosh <hnm-7@yandex.ru>
2020-07-19 00:23:06 +0500

    оверлэй и esc

--- scripts/index.js
+++ scripts/index.js
@@ -57,6 +57,32 @@ closeButtonList.forEach(function(button) {
   })
 });
 
+
+// закрывать попап нажатием на затемненную область
+document.querySelectorAll('.popup').forEach(function(popup) {
+  popup.addEventListener('mousedown',function(evt) {
+    if (evt.target.classList.contains('popup')) {
+      togglePopup(evt.target.closest('.popup'));
+    }
+  })
+});
+
+
+// закрывать попап нажатием на Escape
+document.addEventListener('keydown', function(evt) {
+  const popupOpened = document.querySelector('.popup__opened')
+  if (evt.key === "Escape" && popupOpened) {
+    togglePopup(popupOpened);
+  }
+});
+
+// jobInput.addEventListener('keydown', function(evt) {
+//   if (evt.key === 'Enter') {
+//     editProfile(nameInput.value, jobInput.value);
+//   }
+// })
+
+
 // открыть попап редактирования профиля с сохранением внесенных данных
 function editProfile() {
   nameInput.value = nameProfile.textContent;
@@ -141,3 +167,108 @@ cardForm.addEventListener('submit', function (evt) {
 addButton.addEventListener('click', () => {
   togglePopup(cardPopup);
 });
+
+
+
+
+
+// валидация
+
+const formElement = document.querySelector('.popup__form');
+const formInput = formElement.querySelector('.popup__input');
+// // выбираем элемент ошибки на основе id
+const errorElement = formElement.querySelector(`#${formInput.id}-error`);
+const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+
+// добавляем класс с ошибкой (подчеркивание строки красным) и сообщение об ошибке
+function showInputError(formElement, formInput, errorMessage) {
+  // const errorElement = formElement.querySelector(`#${formInput.id}-error`);
+  formInput.classList.add('popup__input_type_error');
+  // заменим содержимое span с ошибкой на переданный параметр
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+// удаляем класс с ошибкой
+function hideInputError (formElement, formInput) {
+  const errorElement = formElement.querySelector(`#${formInput.id}-error`);
+  formInput.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  // очистим ошибку
+  errorElement.textContent = '';
+}
+
+// проверяем валидность поля
+function isValid (formElement, formInput) {
+  if (!formInput.validity.valid) {
+    // если не проходит валидацию, покажем ошибку
+    showInputError(formElement, formInput, formInput.validationMessage);
+  } else {
+    // ecли проходит валидацию, скроем
+    hideInputError(formElement, formInput);
+  }
+};
+
+function hasInvalidInput (inputList) {
+  return inputList.some((formInput) => {
+    return !formInput.validity.valid;
+  });
+};
+
+function setEventListeners (formElement) {
+
+  const buttonElement = formElement.querySelector('.popup__save');
+  toggleButtonState (inputList, buttonElement);
+  // обходим элементы полученной коллекции
+  inputList.forEach((formInput) => {
+    // каждому полю добавим обработчик события input
+    formInput.addEventListener('input', function () {
+      // внутри колбэка вызовем isValid, передав ей форму и проверяемый элемент
+      isValid(formElement, formInput)
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+// делаем кнопку активной/неактивной
+function toggleButtonState (inputList, buttonElement) {
+ 
+  if (hasInvalidInput(inputList)) {
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.removeAttribute('disabled', true);
+  }
+};
+
+
+
+function enableValidation ({formSelector}) {
+  // найдем все формы с указанным классом в DOM, сделаем из них массив
+  const formList = Array.from(document.querySelectorAll(formSelector));
+  // переберем полученную коллекцию
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    // для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
+    setEventListeners(formElement);
+  });
+};
+
+
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save',
+  inactiveButtonClass: 'popup__save_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+});
+
+
+
+
+
+
+
